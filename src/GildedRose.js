357d4b3e6d70b8170f13d879f0b6b28909cbd9f6
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2018-06-11 16:29:53 +0200

    Small functions for regular and special items update quality

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -8,51 +8,57 @@
 const Item = require('./Item');
 
 class GildedRose {
+
   increaseQuality(item){
     if(item.quality < 50){
       item.quality += 1;
     }
   }
+
   decreaseQuality(item){
     if(item.quality > 0){
       item.quality -= 1;
     }
   }
 
-  updateItemQuality(item){
-
-    if (item.name === "Sulfuras, Hand of Ragnaros") return;
-
-    item.sellIn = item.sellIn - 1;
-
-    if (item.name === "Aged Brie" || item.name === "Backstage passes to a TAFKAL80ETC concert"){
-      if (item.sellIn < 0) {
-        item.quality = 0
-      }else{
+  updateSpecialItemQuality(item){
+    if (item.sellIn < 0) {
+      item.quality = 0
+    }else{
+      this.increaseQuality(item)
+      if (item.sellIn < 11) {
         this.increaseQuality(item)
-        if (item.sellIn < 11) {
-          this.increaseQuality(item)
-        }
-        if (item.sellIn < 6) {
-          this.increaseQuality(item)
-        }
       }
-      return;
+      if (item.sellIn < 6) {
+        this.increaseQuality(item)
+      }
     }
+  }
 
-
+  updateRegularItemQuality(item){
     this.decreaseQuality(item)
 
     if (item.sellIn < 0) {
       this.decreaseQuality(item)
     }
-
   }
-  updateQuality(items) {
-    for (let i = 0; i < items.length; i++) {
-      this.updateItemQuality(items[i]);
+
+  updateItemQuality(item){
+    if (item.name === "Sulfuras, Hand of Ragnaros") return;
+
+    item.sellIn = item.sellIn - 1;
+
+    if (item.name === "Aged Brie" || item.name === "Backstage passes to a TAFKAL80ETC concert"){
+      this.updateSpecialItemQuality(item);
+    }else{
+      this.updateRegularItemQuality(item);
     }
-    return items;
+  }
+
+  updateQuality(items) {
+    items.map(item => this.updateItemQuality(item))
+
+    return items
   }
 
 }
