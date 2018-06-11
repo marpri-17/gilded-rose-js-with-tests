f237757f311fb148f1f6b34c34aa1e844833424c
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2018-06-11 20:59:59 +0200

    Solution of the online session

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -1,77 +1,77 @@
-/*
-    2006-30-84
-    Leeroy was here!!
-
-    Leeroy <lerooy@example.com>
-*/
 
 const Item = require('./Item');
 
+const LEGENDARY_ITEM = "Sulfuras, Hand of Ragnaros";
+const CONJURED_ITEM = "Conjured";
+
+const isSpecialItem = item => item.name === "Aged Brie" || item.name === "Backstage passes to a TAFKAL80ETC concert";
+
+const MAX_QUALITY = 50;
+const MIN_QUALITY = 0;
+
+const DAYS_TO_DOUBLE_QUALITY = 10;
+const DAYS_TO_TRIPLE_QUALITY = 5;
+
 class GildedRose {
+
+  incrementQuality(item, quantity = 1){
+    if(item.quality + quantity < MAX_QUALITY){
+      item.quality += quantity;
+    }else{
+      item.quality = MAX_QUALITY;
+    }
+  }
+
+  decrementQuality(item){
+    if(item.quality > MIN_QUALITY){
+      item.quality -= 1;
+    }
+  }
+
+  updateSpecialItemQuality(item){
+    if (item.sellIn <= DAYS_TO_TRIPLE_QUALITY){
+      this.incrementQuality(item, 3);
+    } else if (item.sellIn <= DAYS_TO_DOUBLE_QUALITY) {
+      this.incrementQuality(item, 2);
+    } else {
+      this.incrementQuality(item);
+    }
+
+    if (item.sellIn < 0) {
+      item.quality = 0;
+    }
+  }
+
+  updateRegularItemQuality(item){
+    this.decrementQuality(item);
+    if (item.sellIn < 0) {
+      this.decrementQuality(item);
+    }
+  }
+
+  updateConjuredItem(item){
+    this.updateRegularItemQuality(item);
+    this.updateRegularItemQuality(item);
+  }
+
+  updateItemQuality(item){
+    if(item.name === LEGENDARY_ITEM) return;
+
+    item.sellIn -= 1;
+
+    if (isSpecialItem(item)) {
+      this.updateSpecialItemQuality(item);
+    } else if(item.name === CONJURED_ITEM){
+      this.updateConjuredItem(item);
+    } else {
+      this.updateRegularItemQuality(item);
+    }
+
+  }
+
   updateQuality(items) {
     for (let i = 0; i < items.length; i++) {
-      if ("Aged Brie" != items[i].name && "Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
-        //TODO: Improve this code.
-        if (items[i].quality > 0) {
-          if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-            items[i].quality = items[i].quality - 1
-          }
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-          if ("Aged Brie" == items[i].name) {
-              if (items[i].sellIn < 6) {
-                items[i].quality = items[i].quality + 1
-              }
-          }
-          //Increases the Quality of the stinky cheese if its 11 days to due date.
-          if ("Aged Brie" == items[i].name) {
-              if (items[i].sellIn < 11) {
-                items[i].quality = items[i].quality + 1
-              }
-          }
-          if ("Backstage passes to a TAFKAL80ETC concert" == items[i].name) {
-            if (items[i].sellIn < 11) {
-              // See revision number 2394 on SVN.
-              if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1
-              }
-            }
-            //Increases the Quality of Backstage Passes if the Quality is 6 or less.
-            if (items[i].sellIn < 6) {
-              if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1
-              }
-            }
-          }
-        }
-      }
-      if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-        items[i].sellIn = items[i].sellIn - 1;
-      }
-      if (items[i].sellIn < 0) {
-        if ("Aged Brie" != items[i].name) {
-          if ("Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
-            if (items[i].quality > 0) {
-              if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-                items[i].quality = items[i].quality - 1
-              }
-            }
-          } else {
-            //TODO: Fix this.
-            items[i].quality = items[i].quality - items[i].quality
-          }
-        } else {
-          if (items[i].quality < 50) {
-            items[i].quality = items[i].quality + 1
-          }
-          if ("Aged Brie" == items[i].name && items[i].sellIn <= 0)
-              items[i].quality = 0;
-        } // of for.
-      }
-      if ("Sulfuras, Hand of Ragnaros" != items[i].name)
-        if (items[i].quality > 50) items[i].quality = 50;
+      this.updateItemQuality(items[i]);
     }
     return items;
   }
